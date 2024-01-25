3e33740c530e3c3ea0003d59213966c4ceb67d6f
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-01-25 17:46:05 -0600

    Add: Exception classes

---
+++ src/main/java/com/ajen/inv/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.ajen.inv.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * 
+ * 
+ * @author ajenk
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
--- src/main/java/com/ajen/inv/exception/ResourceNotFoudException.java
+++ src/main/java/com/ajen/inv/exception/ResourceNotFoudException.java
@@ -1,9 +1,30 @@
 package com.ajen.inv.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.mysql.cj.protocol.Message;
+
 /**
- * 
+ * This class represents an exception that should be thrown when a requested resource is not found.
  * 
  * @author ajenk
  */
-public class ResourceNotFoudException {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoudException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+	public ResourceNotFoudException(String message) {
+		
+		super(message);
+	}
+	
+	
+	
 
 }
