9e2714fa35e25ed5fc1c64ae7f8e2f45774afca1
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-01-29 10:48:26 -0600

    Add: FindAll() JU test

--- src/test/java/com/ajen/inv/service/InventoryServiceTests.java
+++ src/test/java/com/ajen/inv/service/InventoryServiceTests.java
@@ -1,9 +1,86 @@
 package com.ajen.inv.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.ajen.inv.model.InventoryItem;
+import com.ajen.inv.repository.InventoryRepository;
+import com.ajen.inv.service.impl.InventoryServiceImpl;
+
 /**
  * 
  * 
  * @author ajenk
  */
+@ExtendWith(MockitoExtension.class)
 public class InventoryServiceTests {
+	
+	@InjectMocks
+	private InventoryServiceImpl inventoryService;
+	
+	@Mock
+	private InventoryRepository inventoryRepository; 
+	
+	
+	private InventoryItem item;
+	
+	@BeforeEach
+	void setUp() {
+		item = new InventoryItem();
+		item.setItemId(1L);
+		item.setItemName("Test item Name");
+		item.setQuantity("109");
+		item.setLocation("TestLocation");
+	}
+	@Order(1)
+	@DisplayName("when FindAll() ThenReturnsListOfItems")
+	@Test
+	void findAllTest() throws Exception {
+		
+		
+	when(inventoryRepository.findAll()).thenReturn(Arrays.asList(item));
+	
+	List<InventoryItem> results = inventoryService.findAll();
+	
+	assertNotNull(results);
+	assertEquals(1, results.size());
+	verify(inventoryRepository).findAll();
+		
+	}
 
+	@Test
+	void findByIdTest() throws Exception {
+		
+	}
+	
+	@Test
+	void saveTest() throws Exception {
+		
+	}
+	
+	@Test
+	void updateTest() throws Exception {
+		
+	}
+	
+	
+	@Test
+	void deleteTest() throws Exception {
+		
+	}
+	
 }
